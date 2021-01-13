1d3318d9c1c51651e1b708dbf08ab3c3e1d1c1ff
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-13 19:04:37 +0100

    Create second half page content and display icons in header

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,25 +8,48 @@ import {
   SearchBar,
   SearchButton,
   BackgroundImageFirstHalfPage,
+  FirstReminder,
+  SecondReminder,
+  ThirdReminder,
+  Logo,
+  IconContainer,
+  Icon,
+  ButtonSign,
 } from "../styled-components/DashboardStyled";
 
 export default function Dashboard() {
   return (
     <>
       <Main>
-        <Header>Here is the Dashboard</Header>
+        <Header>
+          <Logo>FindMyJob </Logo>
+          <IconContainer>
+            <Icon className="fas fa-user-circle" id="avatar" alt="avatar"></Icon>
+            <Icon className="fas fa-bell" id="bell" alt="notification"></Icon>
+            <Icon className="fas fa-cog" id="options" alt="options"></Icon>
+            <ButtonSign id="sign up">Sign up</ButtonSign>
+            <ButtonSign id="sign in">Sign in</ButtonSign>
+          </IconContainer>
+        </Header>
         <FirstHalfPage>
           <BackgroundImageFirstHalfPage
             src="https://blog.talkspirit.com/wp-content/uploads/2020/05/digital-workplace-1680x840-1.jpg"
             alt="firstHalfPage"
           />
           <SearchSection>
-            <SearchBar id="firstSearchBar"></SearchBar>
-            <SearchBar id="secondSearchBar"></SearchBar>
+            <SearchBar id="firstSearchBar" placeholder="Métier"></SearchBar>
+            <SearchBar
+              id="secondSearchBar"
+              placeholder="Localisation"
+            ></SearchBar>
             <SearchButton>Find</SearchButton>
           </SearchSection>
         </FirstHalfPage>
-        <SecondHalfPage></SecondHalfPage>
+        <SecondHalfPage>
+          <FirstReminder></FirstReminder>
+          <SecondReminder></SecondReminder>
+          <ThirdReminder></ThirdReminder>
+        </SecondHalfPage>
       </Main>
     </>
   );
